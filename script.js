4970074b4244dd1133e4c2a361f626c89ee38078
Padmaj Bharambe <[padmaj.bharambe@gmail.com]>
2024-07-29 16:57:27 +0530

    remove error in code

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function getComputerChoice(){
     var rand=Math.random();
-    console.log(rand);
     if (rand<0.33)
         return "ROCK";
     else if(rand>=0.66)
@@ -13,7 +12,7 @@ console.log(getComputerChoice())
 function getHumanChoice(){
 
     var input=prompt("Enter ROCK, PAPER or SCISSORS");
-    input=input.toUpperCase;
+    input=input.toUpperCase();
 
     if (input=="ROCK"||input=="PAPER"||input=="SCISSORS")
         {
@@ -27,32 +26,69 @@ var humanScore=0, computerScore=0;
 
 
 function playRound(humanChoice,compChoice)
-{
+{   var outcome;
     if (humanChoice=="ROCK")
     {
         if (compChoice=="SCISSORS")
-        return "win";
+        outcome= "win";
         else if (compChoice=="PAPER")
-        return "lose";
+        outcome= "lose";
         else
-        return "tie";
+        outcome= "tie";
     }
     if (humanChoice=="PAPER")
         {
             if (compChoice=="ROCK")
-            return "win";
+            outcome= "win";
             else if (compChoice=="SCISSORS")
-            return "lose";
+            outcome= "lose";
             else
-            return "tie";
+            outcome= "tie";
         }
     if (humanChoice=="SCISSORS")
          {
             if (compChoice=="PAPER")
-            return "win";
+            outcome= "win";
             else if (compChoice=="ROCK")
-            return "lose";
+            outcome= "lose";
             else
-            return "tie";
+            outcome= "tie";
             }  
+    if(outcome=="win")
+        { 
+        console.log("You win! "+humanChoice+" beats "+compChoice);
+        humanScore++;
+        }
+    else if(outcome=="lose")
+        {
+        console.log("You win! "+humanChoice+" beats "+compChoice);
+        computerScore++;
+        }
+            else 
+            console.log("You tied. You and the computer picked the same");
+    console.log("Your score is "+humanScore+" and the computer's score is "+computerScore)
 }
+
+function giveResult(outcome)
+{
+    
+}
+
+function playGame()
+{
+for(var i=0;i<5;i++)
+    {
+    var humanSelection=getHumanChoice();
+    var compSelection=getComputerChoice();
+    var result=playRound(humanSelection,compSelection);
+    
+    }
+}
+//main
+playGame();
+if(humanScore>computerScore)
+    console.log("You won the game");
+else
+    console.log("You lost the game");
+
+
